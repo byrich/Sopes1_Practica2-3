9854d143ddc98aa3fe0004c0da8a19360ca385be
byrich <byrichcraft@gmal.com>
2019-04-04 09:12:21 -0600

    agrego cassandra

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+$cluster   = Cassandra::cluster()
+               ->withContactPoints('192.168.0.8:8083')
+               ->build();
+$session   = $cluster->connect("DB1");
+$statement = new Cassandra\SimpleStatement("SELECT * FROM student");
+$result    = $session->execute($statement);
+echo "Result contains " . $result->count() . " rows";
