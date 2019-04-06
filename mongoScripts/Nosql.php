3f253d1ace253f5c3868f41045863aa28f972738
byrich <byrichcraft@gmail.com>
2019-04-06 09:18:36 +0000

    todo ? 2

---
+++ mongoScripts/Nosql.php
@@ -0,0 +1,38 @@
+<?php
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\BulkWrite;
+use MongoDB\Driver\Query;
+
+$con = new Manager('mongodb://mongodb:27017');
+$miDB = "hola.adios";
+$bw = new BulkWrite();
+$document = array(
+	'texto'=>'un texto'
+);
+$bw->insert($document);
+$con->executeBulkWrite($miDB,$bw);
+$bw1 = new BulkWrite();
+$document = array(
+	'texto'=>'un texto2'
+);
+$bw1->insert($document);
+$con->executeBulkWrite($miDB,$bw1);
+//$con->executeBulkWrite($miDB,$bw);
+//$con->executeBulkWrite($miDB,$bw);
+//$con->executeBulkWrite($miDB,$bw);
+
+
+$res = $con->executeQuery($miDB,new Query([],[]));
+foreach($res as $r)
+{
+	echo $r->texto . "\n";
+}
+echo "----------";
+$bw2 = new BulkWrite();
+$bw2->delete([],['limit' => 0]);
+$con->executeBulkWrite($miDB,$bw2);
+$res = $con->executeQuery($miDB,new Query([],[]));
+foreach($res as $r)
+{
+	echo $r->texto;
+}
